4eeb8746ad0db947fa2c8d021c8f66a8f40684e6
solarslurpi <happyday.mjohnson@gmail.com>
2022-02-04 17:25:21 +0000

    evolved code.py

--- CP_code/code.py
+++ CP_code/code.py
@@ -4,24 +4,36 @@ import time
 from adafruit_debouncer import Debouncer
 from PAR_LIB import PAR
 
+(NOT_CONNECTED,CONNECTED) = range(2)
+
+def button_setup():
+    pin = digitalio.DigitalInOut(board.D0) # Button pin on QT PY ESP32 S2
+    pin.pull = digitalio.Pull.UP
+    return Debouncer(pin)
 
 def bar_graph(read_value):
     scaled = int(read_value / 1000)
     return "[%5d] " % read_value + (scaled * "*")
-    
-def callback_function():
-    print("in callback function")
-    sensor_channels = p.take_reading()
-    print("F1 - 415nm/Violet  %s" % bar_graph(sensor_channels[0]))
-    print("F2 - 445nm//Indigo %s" % bar_graph(sensor_channels[1]))
-    print("F3 - 480nm//Blue   %s" % bar_graph(sensor_channels[2]))
-    print("F4 - 515nm//Cyan   %s" % bar_graph(sensor_channels[3]))
-    print("F5 - 555nm/Green   %s" % bar_graph(sensor_channels[4]))
-    print("F6 - 590nm/Yellow  %s" % bar_graph(sensor_channels[5]))
-    print("F7 - 630nm/Orange  %s" % bar_graph(sensor_channels[6]))
-    print("F8 - 680nm/Red     %s" % bar_graph(sensor_channels[7]))
-    p.send_readings(sensor_channels)
 
+def connect_callback():
+    print("connect_callback")
+    global state
+    state = CONNECTED
+def reading_received_callback():
+    print("reading received")
 
+# We're starting out.  The first thing we need to do is connect to wifi and the
+# mqtt broker.  We use the global state variable to track where we are.
+state = NOT_CONNECTED
+button = button_setup()
 p = PAR()
-p.connect(callback_function)
+p.connect(connect_callback)
+# Once connected, the state will be CONNECTED, since this was set in the callback.
+while True:
+    time.sleep(0.1)
+    if (state == CONNECTED):
+        # If the button is pressed, collect a sample_rate
+        button.update()
+        if button.rose:
+            channel_samples = p.take_reading()
+            p.send_reading(channel_samples,reading_received_callback)
