99f50a3b3698fbb2f070d28b4ec00ad679bde275
solarslurpi <happyday.mjohnson@gmail.com>
2022-02-04 17:33:31 +0000

    updated PAR_LIB.py

--- CP_code/PAR_LIB.py
+++ CP_code/PAR_LIB.py
@@ -14,13 +14,20 @@ class PAR:
         i2c = busio.I2C(board.SCL1,board.SDA1)  # uses board.SCL and board.SDA
         self.sensor = AS7341(i2c)
         self.bConnected = False
+        self.reading_made_it_mqtt_sub = "PAR/READING/OK"
         print(type(self.sensor))
     def _connect_mqtt(self,mqtt_client, userdata, flags, rc):
         # This function will be called when the mqtt_client is connected
         # successfully to the broker.
         self.bConnected = True
         print("Connected to MQTT Broker!")
-        print("Flags: {0}\n RC: {1}".format(flags, rc))      
+        print("Flags: {0}\n RC: {1}".format(flags, rc))
+        # Subscribe to "readings made it" topic feed.
+        self.mqtt_client.subscribe(self.reading_made_it_mqtt_sub)
+    def _message_mqtt(self,mqtt_client,topic,message):
+        print("New message on topic {0}: {1}".format(topic, message))
+        self.send_reading_callback()
+
     def connect(self,callback_function):
         # Get wifi details and more from a secrets.py file
         try:
@@ -43,18 +50,19 @@ class PAR:
             )
         # Connect callback handlers to mqtt_client
         self.mqtt_client.on_connect = self._connect_mqtt
+        self.mqtt_client.on_message = self._message_mqtt
         print("Attempting to connect to %s" % self.mqtt_client.broker)
         self.mqtt_client.connect()
         callback_function()
     def take_reading(self):
         sensor_channels = self.sensor.all_channels
         return [ sensor_channels[i] for i in range(8)]
-    def send_readings(self,readings):
+    def send_reading(self,readings,callback_function):
+        self.send_reading_callback = callback_function
         if (not self.bConnected):
             raise Exception("Please connect first")
-        readingsJson = json.dumps(readings)
+        readingJson = json.dumps(readings)
         self.mqtt_client.loop()
-        self.mqtt_client.publish("PAR/readings",readingsJson)
+        self.mqtt_client.publish("PAR/reading",readingJson)
 
         print("readings sent!")
-
